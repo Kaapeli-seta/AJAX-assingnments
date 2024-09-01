92b44dc6870e9ed9a343f280e75bcc8d24257300
Kaapo <142903354+Kaapeli-seta@users.noreply.github.com>
2024-09-01 17:53:01 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,14 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": ['google', 'eslint:recommended', 'plugin:prettier/recommended'],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-        'require-jsdoc': 0,
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['google', 'eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'require-jsdoc': 0,
+  },
+};
